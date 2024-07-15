84ce31ecd18f7087eac3b5f017941826dc2b6ecf
Shiver1155 <hif7f6dyfdt@gmail.com>
2024-07-14 23:12:47 -0300

    test

--- pythonProject/nn.py
+++ pythonProject/nn.py
@@ -1,16 +1,16 @@
-valores = []
-par = []
-impar = []
+senha = 'senha123'
+tentativas = 0
 while True:
-    n = int(input('Digite um numero'))
-    valores.append(n)
-    if n % 2 == 0:
-        par.append(n)
-    elif n % 2 == 1:
-        impar.append(n)
-    n1 = str(input('Deseja sair? S/N')).upper()
-    if n1 == 'S':
+    n = input('Digite uma senha: ')
+
+    if n == senha:
+        print('senha correta ')
         break
-print(f'A lista de valores {valores}')
-print(f'Essa Lista e par {par}')
-print(f'Essa lista e impar {impar}')
+
+    elif n != senha:
+        print('Senha incorreta')
+        tentativas += 1
+        if tentativas >= 3:
+            print('conta bloqueada')
+            break
+
