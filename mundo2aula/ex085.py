c8f7f7184de90d7d9dea832d9dc6c684c6999cc2
Shiver1155 <hif7f6dyfdt@gmail.com>
2024-08-04 20:50:14 -0300

    consulta_matriz

--- mundo2aula/ex085.py
+++ mundo2aula/ex085.py
@@ -8,6 +8,7 @@ for valor in range(1, 8):
 
     elif n % 2 == 1:
         valores[1].append(n)
-
+valores[0].sort()
+valores[1].sort()
 print(f'Os valores pares sao {valores[0]}')
 print(f'os valores impares sao {valores[1]}')
