55e1ca10fff7503724024bdd4c8188deb3514332
Shiver1155 <hif7f6dyfdt@gmail.com>
2024-10-02 22:11:12 -0300

    Create ex096.py

---
+++ mundo2aula/ex096.py
@@ -0,0 +1,9 @@
+def calcular(larg, comp):
+    area = larg * comp
+    print(f'a area de um terreno {larg}x{comp} e {area}')
+
+
+
+l = float(input('LARGURA: '))
+c = float(input('COMPRIMENTO: '))
+calcular(l,c)
